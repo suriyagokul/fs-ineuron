d06cda3dbb33e19789131d605aaba156503f0d42
suriyagokul <124340052+suriyagokul@users.noreply.github.com>
2023-07-09 11:05:34 +0530

    Create 03_answer.js

---
+++ preplacement/03_Assignment/03_answer.js
@@ -0,0 +1,3 @@
+In responsive web design, breakpoints are specific points or ranges of viewport widths at which the layout and design of a web page are adjusted to provide an optimal viewing experience on different devices and screen sizes. Breakpoints determine when certain CSS styles should be applied or modified using media queries.
+
+Media queries allow you to define different sets of CSS rules that will be applied based on the characteristics of the user's device or browser. Breakpoints are used within media queries to specify the conditions at which these CSS rules should take effect.
